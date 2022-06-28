15dd177c128c76345012587994f494c4702dd281
Kevin <86642014+kevin-on-github@users.noreply.github.com>
2022-06-28 14:56:13 -0400

    upgrade script for Ubuntu 20.04-LTS

--- az-cli-script.sh
+++ az-cli-script.sh
@@ -1,3 +1,4 @@
+#! /bin/bash
 ## Azure CLI commands follow. Will have to have the az cli tools installed.
 ## This can be downloaded from Microsoft, or used directly from the AZ Cloud Shell.
 
@@ -7,14 +8,9 @@
 ## Password complexity is in effect, so 'password' or the like will fail.
 ## Should only take a few minutes to deploy. The output will display the public IP for SSH access.
 
-echo "Enter the Resource Group name:" &&
-read resourceGroupName &&
-echo "Enter the location (i.e. centralus):" &&
-read location &&
-echo "Enter the administrator username:" &&
-read username &&
-echo "Enter the administrator password:" &&
-read -s password &&
-az group create --name $resourceGroupName --location "$location" &&
-az deployment group create --resource-group $resourceGroupName --template-uri https://raw.githubusercontent.com/kevin-on-github/azure-automation/main/create-eve-ng-vm.json --parameters adminUsername=$username adminPasswordOrKey="$password" &&
-az vm show --resource-group $resourceGroupName --name eve-ng-vm  --show-details --output table
+read -p "Name of the new Ubuntu 20.04 LTS virtual machine: " vmName
+read -p "Username of the new virtual machine  [Linux VM names may only contain letters, numbers, '.', and '-'.]: " username
+read -s -p "Password of the vm  [The value must not be empty. Password must be complex: 1 lower case, 1 upper case, and 1 number. Must be between 6 and 72 characters.]: " password
+
+az group create --name $vmName --location eastus
+az deployment group create --resource-group $vmName --template-file create-eve-ng-vm.json --parameters adminUsername=$username adminPassword=$password 
